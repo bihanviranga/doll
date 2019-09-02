324f5a901459a0368078609b61f49c0529d3d1ee
Bihan V de Silva <bihanviranga@gmail.com>
2019-09-02 16:59:59 +0530

    Set so many flags :bulb:

--- doll.c
+++ doll.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 // Store the original terminal settings so we can
@@ -19,13 +21,26 @@ void enableRawMode() {
     // or when program returns from main().
 
     struct termios raw = orig_termios;
-
-    raw.c_lflag &= ~(ECHO);
-    // Symbol ~ means bitwise complement
-    // Symbol & means bitwise 'and'
-    // When '&' with the complement that flag gets turned off
-    // When '|' that flag gets turned on (right?)
-
+    raw.c_iflag &= ~(IXON | ICRNL | INPCK | ISTRIP | BRKINT);
+    // This disables ctrl-s and ctrl-q
+    // which are used to stop and continue data from
+    // being transmitted to the terminal.
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    /* 
+    Symbol ~ means bitwise complement
+    Symbol & means bitwise 'and'
+    When '&' with the complement that flag gets turned off
+    When '|' that flag gets turned on (right?)
+    ECHO means echoing to the terminal.
+    ICANON means canonical mode - reading line-by-line.
+    We want this to read byte-by-byte
+    With ICANON, the program quits as soon as we press 'q'
+    ISIG means signals like SIGINT (ctrl-c) or SIGTSTP (ctrl-z).
+    We want those ignored.
+     */
+    
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     // Set the new attributes
     // TCSAFLUSH and stuff are in the manpage for termios
@@ -35,6 +50,13 @@ int main() {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        }
+        else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
     return 0;
 }
