47cf22663a511d10795bc88bbc8b95bc4286c056
Bihan V de Silva <bihanviranga@gmail.com>
2019-09-03 11:09:33 +0530

    :sparkles: Home, Delete, and End keys

--- doll.c
+++ doll.c
@@ -26,13 +26,16 @@ ASCII has 7 bits.
 // This acts as a constructor to abuf type
 
 enum editorKey {
+    // This is 1000 so it doesn't interefere with char values of keypresses
     ARROW_UP = 1000,
     ARROW_DOWN,
     ARROW_LEFT,
     ARROW_RIGHT,
     PAGE_UP,
-    PAGE_DOWN
-    // This is 1000 so it doesn't interefere with char values of keypresses
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
 };
 
 /*** data ***/
@@ -117,8 +120,13 @@ int editorReadKey() {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                        case '1' : return HOME_KEY;
+                        case '3' : return DEL_KEY;
+                        case '4' : return END_KEY;
                         case '5' : return PAGE_UP;
                         case '6' : return PAGE_DOWN;
+                        case '7' : return HOME_KEY;
+                        case '8' : return END_KEY;
                     }
                 }
             }
@@ -128,7 +136,15 @@ int editorReadKey() {
                     case 'B' : return ARROW_DOWN;
                     case 'C' : return ARROW_RIGHT;
                     case 'D' : return ARROW_LEFT;
+                    case 'H' : return HOME_KEY;
+                    case 'F' : return END_KEY;
                 }
+            }
+        }
+        else if (seq[0] == '0') {
+            switch (seq[1]) {
+                case 'H' : return HOME_KEY;
+                case 'F' : return END_KEY;
             }
         }
         return '\x1b';
@@ -294,6 +310,13 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
 
         case PAGE_UP:
