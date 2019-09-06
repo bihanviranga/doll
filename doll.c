e5f06b664ef3dfe534dcceac4389bbb4cf398223
Bihan V de Silva <bihanviranga@gmail.com>
2019-09-06 10:57:56 +0530

    :sparkles: Implemented vertical scrolling

--- doll.c
+++ doll.c
@@ -52,11 +52,12 @@ typedef struct erow {
 } erow;
 
 struct editorConfig {
-    int cx, cy;
-    int screenrows;
-    int screencols;
-    int numrows;
-    erow *row;
+    int cx, cy; // cursor position
+    int rowoff; // row offset
+    int screenrows; // num of rows in screen
+    int screencols; // num of cols in screen
+    int numrows; // num of rows in file
+    erow *row; // array of rows
     struct termios orig_termios;
     // Store the original terminal settings so we can
     // restore them on exit.
@@ -260,10 +261,20 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+    if (E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        if (y >= E.numrows) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -282,9 +293,9 @@ void editorDrawRows(struct abuf *ab) {
             }
         }
         else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -301,6 +312,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -315,7 +328,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     // The terminal libraries start indexing rows/cols from 1.
     abAppend(&ab, buf, strlen(buf));
 
@@ -341,7 +354,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_DOWN:
-            if (E.cy != E.screenrows - 1) {
+            if (E.cy < E.numrows) {
                 E.cy++;
             }
             break;
@@ -395,6 +408,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
 
